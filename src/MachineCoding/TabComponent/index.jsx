dc66a57d2f00bee8cfd8357b4cf571066e427b48
saurav12797 <36052802+saurav12797@users.noreply.github.com>
2025-06-29 03:20:48 +0000

    Update edge scenarios for Tab Component

--- src/MachineCoding/TabComponent/index.jsx
+++ src/MachineCoding/TabComponent/index.jsx
@@ -4,9 +4,9 @@ import "./style.css";
 export default function TabComponent() {
   const [activeTab, setActiveTab] = useState(1);
   const tabs = [
-    { name: "Tab1", id: 1, component: <div>Tab 1 component</div> },
-    { name: "Tab2", id: 2, component: <div>Tab 2 component</div> },
-    { name: "Tab3", id: 3, component: <div>Tab 3 component</div> },
+    { name: "Profile", id: 1, component: <div>Tab 1 component</div> },
+    { name: "Settings", id: 2, component: <div>Tab 2 component</div> },
+    { name: "Details", id: 3 },
   ];
 
   const handleTabClick = (id) => setActiveTab(id);
@@ -15,21 +15,22 @@ export default function TabComponent() {
     <div>
       <h1>Tab Component </h1>
       <div className="tab-container">
-        {tabs?.map((item) => {
+        {tabs?.map((item, index) => {
           return (
             <div className="tab-component" key={item.id}>
               <button
                 onClick={() => handleTabClick(item.id)}
                 className={`tab-btn ${activeTab == item.id && "active"}`}
               >
-                {item?.name}
+                {item?.name ?? `Tab${index + 1}`}
               </button>
             </div>
           );
         })}
       </div>
       <div className="tab-content">
-        {tabs.find((tab) => tab.id === activeTab)?.component}
+        {tabs.find((tab) => tab.id === activeTab)?.component ??
+          "No content available"}
       </div>
     </div>
   );
