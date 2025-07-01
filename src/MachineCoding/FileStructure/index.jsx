94f817f37357bf6def765d43d5550932aa2c1179
saurav12797 <36052802+saurav12797@users.noreply.github.com>
2025-07-01 04:56:02 +0000

    File Explorer CRUD

--- src/MachineCoding/FileStructure/index.jsx
+++ src/MachineCoding/FileStructure/index.jsx
@@ -2,6 +2,7 @@ import "./style.css";
 import { data } from "./json";
 import { useState } from "react";
 const FileStructure = () => {
+  const [itemData, setItemData] = useState(data);
   const [isExpanded, setIsExpanded] = useState({});
 
   const handleExpandCollapse = (node) => {
@@ -11,24 +12,42 @@ const FileStructure = () => {
     }));
   };
 
-   const ListContent = ({ data }) => {
+  const ListContent = ({ data, addNodeToList, deleteNodeFromList }) => {
     return (
       <div className="list-content">
         {data?.map((node) => (
           <div className="list-container" key={node.id}>
-            <p>
+            <div className="list-heading">
               {node.isFolder && (
                 <span
                   className="expand-collapse"
                   onClick={() => handleExpandCollapse(node)}
                 >
-                  +
+                  {isExpanded[node.name] ? "-" : "+"}
                 </span>
               )}
               {node.name}
-            </p>
+              {node.isFolder && (
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1091/1091916.png"
+                  width={23}
+                  height={20}
+                  onClick={() => addNodeToList(node.id)}
+                />
+              )}
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+                width={23}
+                height={20}
+                onClick={() => deleteNodeFromList(node.id)}
+              />
+            </div>
             {node.children && isExpanded[node.name] && (
-              <ListContent data={node.children} />
+              <ListContent
+                data={node.children}
+                addNodeToList={addNodeToList}
+                deleteNodeFromList={deleteNodeFromList}
+              />
             )}
           </div>
         ))}
@@ -36,11 +55,52 @@ const FileStructure = () => {
     );
   };
 
+  const addNodeToList = (nodeId) => {
+    const name = prompt("Enter folder name");
+    const updateNode = (list) => {
+      return list?.map((node) => {
+        if (node.id == nodeId) {
+          return {
+            ...node,
+            children: [
+              ...(node.children || []),
+              { name: name, id: Date.now(), isFolder: false },
+            ],
+          };
+        }
+        // if it has more children
+        return {
+          ...node,
+          children: updateNode(node.children),
+        };
+      });
+    };
+
+    setItemData((prev) => updateNode(prev));
+  };
+
+  const deleteNodeFromList = (nodeId) => {
+    const deleteNode = (list) => {
+      return list
+        ?.filter((node) => node.id !== nodeId) // Remove the matching node
+        .map((node) => ({
+          ...node,
+          children: deleteNode(node.children), // Recurse into children
+        }));
+    };
+
+    setItemData((prev) => deleteNode(prev));
+  };
+
   return (
     <div>
       <h1>File Structure</h1>
       <div className="file-structure">
-        <ListContent data={data} />
+        <ListContent
+          data={itemData}
+          addNodeToList={addNodeToList}
+          deleteNodeFromList={deleteNodeFromList}
+        />
       </div>
     </div>
   );
