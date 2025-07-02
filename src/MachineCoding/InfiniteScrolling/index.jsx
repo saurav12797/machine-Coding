89ac8e2e435da387588ef1eb2fd586bedcd7600e
Saurav Kumar <36052802+saurav12797@users.noreply.github.com>
2025-07-02 04:51:39 +0000

    Add Infinite scroll without API

--- src/MachineCoding/InfiniteScrolling/index.jsx
+++ src/MachineCoding/InfiniteScrolling/index.jsx
@@ -2,21 +2,35 @@ import { useState } from "react";
 import "./style.css";
 
 const InfiniteScrolling = () => {
-  const [data] = useState([...Array(10).keys()]);
+  const [data, setData] = useState([...Array(10)]);
+  const [loading, setLoading] = useState(false);
+  const THRESHOLD = 100;
+
+  const loadMoreData = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setData((prev) => [...prev, ...Array(10)]);
+      setLoading(false);
+    }, 1000);
+  };
 
   const handleScroll = (e) => {
-    const topHeight = e.target.scrollTop;
-    console.log(topHeight);
+    const { scrollTop, clientHeight, scrollHeight } = e.target;
+    const remainingHeight = scrollHeight - (scrollTop + clientHeight);
+    if (remainingHeight < THRESHOLD && !loading) {
+      loadMoreData();
+    }
   };
 
   return (
     <div className="scroll-container" onScroll={handleScroll}>
       <h1>Infinite Scroll</h1>
-      {data.map((item, index) => (
+      {data?.map((item, index) => (
         <div className="scrolling-wrapper" key={index}>
           <p>{item}</p>
         </div>
       ))}
+      {loading && <div>Loading....</div>}
     </div>
   );
 };
